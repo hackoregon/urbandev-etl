347d3c3b447409a17ea4a50b91f2d26248df9ddd
Jim Tyhurst <jim@tyhurst.com>
2016-04-03 15:04:38 -0700

    Fixed documentation in deoid10.sh

--- postgresql/scripts/bin/geoid10.sh
+++ postgresql/scripts/bin/geoid10.sh
@@ -3,15 +3,12 @@
 # concatenating the STATEA + COUNTYA + TRACTA + BLCK_GRPA fields.
 # A GEOID10 property appears in Block Group *.shp files, so this new
 # column in the CSV file can be used to select data by Block Group.
-# Other clean-up:
-#  Removes the decimal point from the first data column, so that
-#  it will be treated as an integer.
 # Limitations:
 #   This script only works for CSV files that were downloaded from nhgis.com
-#   with only 'Block Group' selected as the 'Geographic Level'.
+#   with only 'Block Group' selected for the 'Geographic Level'.
 # Usage:
 #   Reads from stdin
 #   Writes to stdout
 # Example:
-#   $ geoid10.sh < population_by_blkgrp.csv > population_by_blkgrp-geoid10.csv
+#   $ geoid10.sh < nhgis0008_ts_geog2010_blckgrp.csv > census_total_population_blkgrp.csv
 awk -F, '{printf("%s,%s%s%s%s\n", $0, $4, $6, $7, $8)}' | sed 's/""//g' | sed 's/STATEACOUNTYATRACTABLCK_GRPA/GEOID10/g'
