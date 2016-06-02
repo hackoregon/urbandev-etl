d74a938fd5db734f7bfb5f97b771cf9d4c38407f
Jeffrey Meyers <jeff@m4>
2016-06-02 10:30:05 -0700

    refactor generate script to handle connection errors and print better messages

--- json/generate/generate.py
+++ json/generate/generate.py
@@ -18,14 +18,18 @@ available inside the `OUTPUT` directory
 
 import json
 import os
+import sys
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import OperationalError
 
 
 # configuration screen to connect to urbandev postgres database
 # 'postgresql://<user>:<password>@<host>:<port>/<database>'
-PG_CONFIG = 'postgresql://jeff:password@localhost:5432/urbandev'
+# leave user, password, host and port empty to connect using peer authentication
+PGCONFIG_ENVVAR = 'URBANDEV_PGCONFIG'
+PGCONFIG = os.getenv(PGCONFIG_ENVVAR, 'postgresql://@/urbandev')
 # path to json output directory
 OUTPUT = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'output')
 
@@ -33,6 +37,17 @@ OUTPUT = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'output')
 def initialize_session(config):
     """ connect to postgres database and return session used for queries """
     engine = create_engine(config)
+    try:
+        # make sure we can connect to database using config string
+        connection = engine.connect()
+        connection.close()
+    except OperationalError as error:
+        # connection failed so print error message and exit
+        msg = "{0}\nFailed to open connection to database.\n"
+        msg += "Try setting `{1}` environment variable, or\n"
+        msg += "modifying `PGCONFIG` variable with valid connection string"
+        print msg.format(error, PGCONFIG_ENVVAR)
+        sys.exit(1)
     session = sessionmaker(bind=engine)
     return session()
 
@@ -46,14 +61,16 @@ def generate_json(session, output_directory):
     """ generate json for each Zillow neighborhood """
     for record in session.execute("SELECT * FROM zillow_json"):
         filename = "{regionid}.json".format(regionid=record['regionid'])
-        with open(os.path.join(output_directory, filename), 'w') as jsonfile:
+        output = os.path.join(output_directory, filename)
+        print "{0}: {1}".format(record['name'], output)
+        with open(output, 'w') as jsonfile:
             jsondata = dump_pretty_json(record['json'])
             jsonfile.write(jsondata)
 
 
 def main():
     """ main function to generate static json for each Zillow neighborhood """
-    session = initialize_session(PG_CONFIG)
+    session = initialize_session(PGCONFIG)
     generate_json(session, OUTPUT)
 
 
