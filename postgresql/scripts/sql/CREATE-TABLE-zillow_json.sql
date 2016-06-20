b0097a75f55b79ca5e54028a005f04187c6d820c
Jeffrey Meyers <jeff@m4>
2016-06-19 19:10:23 -0700

    update zillow_json to include zillow data instead of using values from python script

--- postgresql/scripts/sql/CREATE-TABLE-zillow_json.sql
+++ postgresql/scripts/sql/CREATE-TABLE-zillow_json.sql
@@ -1,5 +1,63 @@
 CREATE TABLE zillow_json AS
-WITH demolitions_json AS (
+WITH median_value_sqft_json AS (
+    SELECT
+        regionid,
+        json_build_object(
+            'Months', json_agg(month),
+            'Values', json_agg(MedianValue_sqft)
+        ) AS data
+    FROM (
+        SELECT regionid, month, MedianValue_sqft
+        FROM zillow_1996_2016
+        ORDER BY month
+    ) AS sq
+    GROUP BY regionid
+), median_sold_sqft_json AS (
+    SELECT
+        regionid,
+        json_build_object(
+            'Months', json_agg(month),
+            'Values', json_agg(MedianSold_sqft)
+        ) AS data
+    FROM (
+        SELECT regionid, month, MedianSold_sqft
+        FROM zillow_1996_2016
+        ORDER BY month
+    ) AS sq
+    GROUP BY regionid
+), zri_sqft_json AS (
+    SELECT
+        regionid,
+        json_build_object(
+            'Months', json_agg(month),
+            'Values', json_agg(ZRI_sqft)
+        ) AS data
+    FROM (
+        SELECT regionid, month, ZRI_sqft
+        FROM zillow_1996_2016
+        ORDER BY month
+    ) AS sq
+    GROUP BY regionid
+), craigslist_json AS (
+    SELECT
+        regionid,
+        json_build_object(
+            'Values', json_build_object(
+                'num_of_studio', num_of_studio,
+                'avg_price_studio', avg_price_studio,
+                'num_of_1_br', num_of_1_br,
+                'avg_price_1_br', avg_price_1_br,
+                'num_of_2_br', num_of_2_br,
+                'avg_price_2_br', avg_price_2_br,
+                'num_of_3_br', num_of_3_br,
+                'avg_price_3_br', avg_price_3_br,
+                'avg_price', avg_price,
+                'rentability', rentability,
+                'listing_count', listing_count
+            )
+        ) AS data
+    FROM craigslist_neighborhood_average_rental AS c
+), demolitions_json AS (
     SELECT
         regionid,
         json_build_object(
@@ -21,6 +79,7 @@ WITH demolitions_json AS (
             'nonviolent', nonviolent
         ) AS violent
         FROM zillow_violent_crime
+        ORDER BY year
     ) AS sq
     GROUP BY regionid
 ), crime_json AS (
@@ -61,6 +120,7 @@ WITH demolitions_json AS (
             'weapons', weapons
         ) AS offense
         FROM zillow_crime
+        ORDER BY year
     ) AS sq
     GROUP BY regionid
 ), race_json AS (
@@ -82,6 +142,7 @@ WITH demolitions_json AS (
             'multiple', multiple
         ) AS race
         FROM zillow_race
+        ORDER BY year
     ) AS sq
     GROUP BY regionid
 ), income_json AS (
@@ -106,6 +167,7 @@ WITH demolitions_json AS (
             'income_200000_more', income_200000_more
         ) AS income
         FROM zillow_income
+        ORDER BY year
     ) AS sq
     GROUP BY regionid
 ), education_json AS (
@@ -125,6 +187,7 @@ WITH demolitions_json AS (
             'advanced', advanced
         ) AS education
         FROM zillow_education
+        ORDER BY year
     ) AS sq
     GROUP BY regionid
 )
@@ -134,17 +197,29 @@ SELECT
     json_build_object(
         'RegionID', nbo.regionid,
         'RegionName', nbo.name,
+        'Overview', o.overview,
+        'Craigslist', cr.data,
         'Demolitions', d.data,
         'ViolentCrime', v.data,
         'Crime', c.data,
         'Race', r.data,
         'Income', i.data,
-        'Education', e.data
+        'Education', e.data,
+        'Zillow', json_build_object(
+            'MedianValue_sqft', mv.data,
+            'MedianSold_sqft', ms.data,
+            'ZRI_sqft', zr.data
+        )
     ) AS json
 FROM (
     SELECT DISTINCT name, regionid
     FROM zillow_neighborhoods
 ) AS nbo
+LEFT OUTER JOIN zillow_overview AS o ON nbo.regionid::text = o.regionid::text
+LEFT OUTER JOIN median_value_sqft_json AS mv ON nbo.regionid::text = mv.regionid::text
+LEFT OUTER JOIN median_sold_sqft_json AS ms ON nbo.regionid::text = ms.regionid::text
+LEFT OUTER JOIN zri_sqft_json AS zr ON nbo.regionid::text = zr.regionid::text
+LEFT OUTER JOIN craigslist_json AS cr ON nbo.regionid::text = cr.regionid::text
 LEFT OUTER JOIN demolitions_json AS d ON nbo.regionid::text = d.regionid::text
 LEFT OUTER JOIN violent_crime_json AS v ON nbo.regionid::text = v.regionid::text
 LEFT OUTER JOIN crime_json AS c ON nbo.regionid::text = c.regionid::text
@@ -152,3 +227,4 @@ LEFT OUTER JOIN race_json AS r ON nbo.regionid::text = r.regionid::text
 LEFT OUTER JOIN income_json AS i ON nbo.regionid::text = i.regionid::text
 LEFT OUTER JOIN education_json AS e ON nbo.regionid::text = e.regionid::text
 ORDER BY nbo.name, nbo.regionid;
+
